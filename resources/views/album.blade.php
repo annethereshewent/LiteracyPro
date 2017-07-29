0a948118cc4c1cf811b177c83d286c4abc485dfe
Anne Castrillon <anne.castrillon@gmail.com>
2017-07-28 22:51:09 -0700

    Fixed date formatting in album page

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -33,8 +33,8 @@
 					<td class="button"><a href="" onclick="delete_element({{$album->id}}, 'album');return false;"><img src="{{asset('images/delete.png')}}" class="button-img"></a></td>
 					<td class="button"><a href="/album_details/update/{{$album->id}}?previous=album"><img src="{{asset('images/edit.png')}}" class="button-img"></a></td>
 					<td><a href="/album_details/view/{{$album->id}}">{{$album->name}}</a></td>
-					<td>{{$album->recorded_date}}</td>
-					<td>{{$album->release_date}}</td>
+					<td>{{date('F jS Y',strtotime($album->recorded_date))}}</td>
+					<td>{{date('F jS Y', strtotime($album->release_date))}}</td>
 					<td>{{$album->number_of_tracks}}</td>
 					<td>{{$album->label}}</td>
 					<td>{{$album->producer}}</td>
